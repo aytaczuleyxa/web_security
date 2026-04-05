09d7505c3e44f8b2026902b816b0d5144b5ebd9b
aytaczuleyxa <rustamliaytac13@gmail.com>
2026-04-05 19:50:03 +0400

    Create kitabxana_db.sql

---
+++ kitabxana_db.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS kitabxana_db;
+USE kitabxana_db;
+
+CREATE TABLE IF NOT EXISTS kitablar (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    author VARCHAR(255)
+);
+
+INSERT INTO kitablar (name, author) VALUES 
+('Sefiller', 'Viktor Huqo'),
+('1984', 'George Orwell'),
+('Cinayet ve Ceza', 'Fyodor Dostoyevski');
+('Anne Karenina', 'Lev Tolstoy'),
+('Martin Iden', 'Cek London'),
+('İnsan nədir', 'Mark Tven'),
+('Drakula', 'Brem Stoker'),
